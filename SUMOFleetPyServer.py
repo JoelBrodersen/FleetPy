704e203d6ee70f192f4aaeff6f0f33b025848c05
JoelBrodersen <117082386+JoelBrodersen@users.noreply.github.com>
2025-12-08 14:21:26 +0100

    Update SUMOFleetPyServer.py

--- SUMOFleetPyServer.py
+++ SUMOFleetPyServer.py
@@ -406,7 +406,7 @@ class SUMOFleetPyServer():
                     if loaded_vehicle not in vehicles_from_main:
                         traci.vehicle.remove(loaded_vehicle)
                             
-                if branch_step%1==0:
+                if branch_step > 0:
                     sim_pos_dict_branch,res_list_branch = self._get_current_edge_tt(sim_time=start_step + branch_step ,sim_pos_dict=sim_pos_dict_branch,res_list=res_list_branch)
 
                 traci.simulationStep()
