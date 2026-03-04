ed9516a5f67ddf59452737c6f87fcc13638788c0
JoelBrodersen <117082386+JoelBrodersen@users.noreply.github.com>
2026-03-04 17:11:31 +0100

    Added op_max_wait_time as Parameter

--- SimulationRunner.py
+++ SimulationRunner.py
@@ -76,7 +76,7 @@ class SimulationRunner:
             sc_df["reliable_tt_prob"] = [int(row["reliable_tt_prob"])]
             sc_df["k_quantile"] = [float(row["k_quantile"])]
             sc_df["rtt_corr_add"] = [float(row["rtt_corr_add"])]
-
+            sc_df["op_max_wait_time"] = [float(row["op_max_wait_time"])]
             self.sc_config_file_dict.update({sc_index:sc_df.squeeze()})
             sc_df.to_csv(py_path.parent/"studies"/self.study_name/"scenarios"/f"{scenario_name}.csv", index=False)
 
