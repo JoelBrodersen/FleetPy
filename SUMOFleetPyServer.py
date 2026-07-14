0a220610ca92f3aa882b157bae45426e5ff30841
JoelBrodersen <117082386+JoelBrodersen@users.noreply.github.com>
2026-07-14 16:45:08 +0200

    Update SUMOFleetPyServer.py

--- SUMOFleetPyServer.py
+++ SUMOFleetPyServer.py
@@ -544,7 +544,7 @@ class SUMOFleetPyServer():
                                 #print(f"Updated new route for {sumo_vid} after removing the first edge: {sumo_route.SUMO_route_edges}")
                                 #breakpoint()
                             print(f"Old route for {sumo_vid}: {traci.vehicle.getRoute(sumo_vid)}")
-                            traci.vehicle.setRoute(sumo_vid,str(sumo_route.SUMO_route_edges))
+                            traci.vehicle.setRoute(sumo_vid,list(sumo_route.SUMO_route_edges))
                             traci.vehicle.setParameter(objectID=sumo_vid, key="arrivalPos", value=str(sumo_route.arrivalPos))
                             traci.vehicle.setParameter(objectID=sumo_vid, key="departPos", value=str(sumo_route.departPos))
                             for e1, e2 in zip(sumo_route.SUMO_route_edges[0:-1], sumo_route.SUMO_route_edges[1:-1]):
