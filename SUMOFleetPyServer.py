11b56612e20d7d53f98c2bccc13f96fd3d497557
JoelBrodersen <117082386+JoelBrodersen@users.noreply.github.com>
2026-07-16 15:40:32 +0200

    Update SUMOFleetPyServer.py

--- SUMOFleetPyServer.py
+++ SUMOFleetPyServer.py
@@ -432,6 +432,7 @@ class SUMOFleetPyServer():
             step+=1
         traci.close()
         print(f"SUMO-Route used for rerouting: {len(self.sumo_router_usage)} times")
+        LOG.warning(f"FleetPy-Routing used for rerouting: {self.FP_router_usage} times")
         self._post_sim_evaluation()
 
     def _post_sim_evaluation(self):
