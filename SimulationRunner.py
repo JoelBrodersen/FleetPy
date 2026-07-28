de8edae4a60eefc56f18c1f87556c4d482714d14
JoelBrodersen <117082386+JoelBrodersen@users.noreply.github.com>
2026-07-28 14:46:58 +0200

    Update SimulationRunner.py

--- SimulationRunner.py
+++ SimulationRunner.py
@@ -148,7 +148,7 @@ class SimulationRunner:
             str(self.py_path.parent/"studies"/self.study_name/"scenarios"/"constant_config.csv"),
             str(self.py_path.parent/"studies"/self.study_name/"scenarios"/f"{self.sc_config_file_dict[sc_index]['scenario_name']}.csv"),
             str(sumocfg_path),
-            "sumo",
+            "sumo-gui",
             "info",
             self.sumo_api
         ]
